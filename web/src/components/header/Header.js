81f97885a8f90fe1b9895701ffd5d11bb9035b2e
radson.lima <radson.lima@grupoicts.com.br>
2022-11-11 17:00:33 -0300

    upando programa

---
+++ web/src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import * as C from './styles'
+
+const Header = () => {
+    return (
+        <C.Container>
+            <C.Header>
+                <C.Title>controle financeiro</C.Title>
+            </C.Header>
+        </C.Container>
+    )
+}
+
+export default Header
---
+++ web/src/components/resume/Resume.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ResumeItem from '../resumeItem/ResumeItem';
+import * as C from './Styles'
+import { FaRegArrowAltCircleUp, FaRegArrowAltCircleDown, FaDollarSign } from 'react-icons/fa'
+
+const Resume = ({ income, expense, total }) => {
+    return (
+        <C.Container>
+            <ResumeItem title="Entradas" Icon={FaRegArrowAltCircleUp} value={income}/>
+            <ResumeItem title="Saídas" Icon={FaRegArrowAltCircleDown} value={expense} />
+            <ResumeItem title="Total" Icon={FaDollarSign} value={total} />
+        </C.Container>
+    )
+}
+
+export default Resume;
